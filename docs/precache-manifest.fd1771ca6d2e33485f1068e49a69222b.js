20765502f8a64c9f91e1961654a13cad17fd2bbd
vincemis610 <vincemis610@gmail.com>
2020-08-20 18:13:55 -0500

    docs/ agregado

---
+++ docs/precache-manifest.fd1771ca6d2e33485f1068e49a69222b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06c5d6baccfef9a5f4ccb45c08395eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cab51bcdfbed538d2366",
+    "url": "/static/css/main.71154c62.chunk.css"
+  },
+  {
+    "revision": "3aabba2936daffd6e9e7",
+    "url": "/static/js/2.e1e62ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1e62ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab51bcdfbed538d2366",
+    "url": "/static/js/main.e466c675.chunk.js"
+  },
+  {
+    "revision": "ae8d6b3161fdf99378b7",
+    "url": "/static/js/runtime-main.7276145b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
